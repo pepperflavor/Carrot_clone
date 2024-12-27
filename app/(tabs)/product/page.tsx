31e7cb83790ba3c55ff4f68c0dde7f7d48c89892
summerjin <j8747j@gmail.com>
2024-12-28 05:07:32 +0900

    tab-bar

---
+++ app/(tabs)/product/page.tsx
@@ -0,0 +1,12 @@
+async function getProduct() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+}
+
+export default async function Product() {
+  const products = await getProduct();
+  return (
+    <div>
+      <h1 className="text-white text-4xl">Product!</h1>
+    </div>
+  );
+}
