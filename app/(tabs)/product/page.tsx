d2e6a316eb21a6e7424372e38f19db934f30e546
summerjin <j8747j@gmail.com>
2024-12-28 05:52:41 +0900

    list

--- app/(tabs)/product/page.tsx
+++ app/(tabs)/product/page.tsx
@@ -1,12 +1,29 @@
+import ListProduct from "@/components/list-product";
+import db from "@/lib/db";
+
 async function getProduct() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  // await new Promise((resolve) => setTimeout(resolve, 10000));
+  const product = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+  });
+  return product;
 }
 
 export default async function Product() {
   const products = await getProduct();
   return (
-    <div>
-      <h1 className="text-white text-4xl">Product!</h1>
+    <div className="p-5 flex flex-col gap-5">
+      <h1 className="text-white text-4xl">
+        {products.map((product) => (
+          <ListProduct key={product.id} {...product} />
+        ))}
+      </h1>
     </div>
   );
 }
